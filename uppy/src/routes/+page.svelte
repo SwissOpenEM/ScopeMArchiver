c94546f2c637b89271fddb85f19f36e46da09371
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-02-12 17:17:00 +0100

    Add basic Svelte project with smui-theme

---
+++ uppy/src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Button, { Group, Label } from '@smui/button';
+</script>
+
+<h1>Open EM Network Data Uploader Service</h1>
+
+<center>
+	<Group variant="raised">
+		<Button href="http://openem-dev.ethz.ch/minio/browser" target="_blank" class="button-shaped-round">
+			<Label>Minio</Label>
+		</Button>
+
+		<Button
+			variant="outlined"
+			class="button-shaped-round"
+			href="http://openem-dev.ethz.ch/traefik"
+			target="_blank"
+		>
+			<Label>Traefik</Label>
+		</Button>
+
+		<Button variant="outlined" class="button-shaped-round" href="http://openem-dev.ethz.ch/grafana">
+			<Label>Grafana</Label>
+		</Button>
+
+		<Button variant="outlined" class="button-shaped-round" href="/tusd-upload">
+			<Label>Tusd Upload</Label>
+		</Button>
+
+		<Button variant="outlined" class="button-shaped-round" href="/client-to-store-upload">
+			<Label>Minio Direct Upload</Label>
+		</Button>
+	</Group>
+</center>
+
+
+
