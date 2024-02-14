fe71c646b3e6fca21c2ab34f46d368618449b92e
phwissmann <33758534+phwissmann@users.noreply.github.com>
2024-02-14 15:37:50 +0100

    Add tusd upload (#10)

--- webuploader/src/routes/+page.svelte
+++ webuploader/src/routes/+page.svelte
@@ -10,24 +10,20 @@
 			<Label>Minio</Label>
 		</Button>
 
-		<Button
-			variant="outlined"
-			class="button-shaped-round"
-			href="/traefik"
-			target="_blank"
-		>
-			<Label>Traefik</Label>
+		<Button variant="outlined" class="button-shaped-round" href="/traefik" target="_blank">
+			<Label>Traefik Dashboard</Label>
 		</Button>
 
 		<Button variant="outlined" class="button-shaped-round" href="grafana">
 			<Label>Grafana</Label>
 		</Button>
 
+		<Button variant="outlined" class="button-shaped-round" href="/tusd-upload">
+			<Label>TuS File Upload</Label>
+		</Button>
+
 		<Button variant="outlined" class="button-shaped-round" href="/s3-upload">
-			<Label>File Upload to Minio</Label>
+			<Label>S3 File Upload</Label>
 		</Button>
 	</Group>
 </center>
-
-
-
