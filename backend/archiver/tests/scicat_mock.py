8f98fc0b1aecb791c0c72921c6dccb6390c8ef86
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-03-26 16:24:30 +0100

    Add scicat API mock

---
+++ backend/archiver/tests/scicat_mock.py
@@ -0,0 +1,23 @@
+import requests_mock
+import archiver.tasks as tasks
+
+
+class ScicatMock(requests_mock.Mocker):
+    ENDPOINT = "mock://scicat.example.com"
+
+    def __init__(self, job_id: int, dataset_id: int):
+        super().__init__()
+        self.matchers: dict[str, requests_mock.Request.matcher] = {}
+
+        self.matchers["jobs"] = self.patch(
+            f"{self.ENDPOINT}/{tasks.scicat.API}/Jobs/{job_id}", json=None)
+        self.matchers["datasets"] = self.post(
+            f"{self.ENDPOINT}/{tasks.scicat.API}/Datasets/{dataset_id}", json=None)
+
+    @property
+    def jobs_matcher(self):
+        return self.matchers["jobs"]
+
+    @property
+    def datasets_matcher(self):
+        return self.matchers["datasets"]
