309b4310644a0d54e5066c1ad37de8110eee91a3
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-07-25 14:03:58 +0200

    BackendAPI: provide endpoint for scicat
    The endpoint expects only job_id and type for now

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,5 +1,9 @@
+from uuid import UUID
+import requests
+from archiver.scicat
 import asyncio
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+from typing import Any, List
 from fastapi.responses import JSONResponse
 from prefect.deployments.deployments import run_deployment
 
@@ -8,69 +12,67 @@ from archiver.utils.model import StorageObject, Job
 
 router = APIRouter()
 
-# Deployment function
 
+async def run_archiving_deployment(job_id: UUID, dataset_list: List[str]):
 
-async def run_archiving_deployment(job: Job):
     a = await asyncio.create_task(run_deployment("archive_datasetlist/datasets_archival", parameters={
-        "dataset_ids": [d.pid for d in job.datasetList or []],
-        "job_id": job.id
+        "dataset_ids": dataset_list,
+        "job_id": job_id
     }, timeout=0))
     return a
 
 
-async def run_retrieval_deployment(job: Job):
+async def run_retrieval_deployment(job_id: UUID, dataset_list: List[str]):
     a = await asyncio.create_task(run_deployment("retrieve_datasetlist/dataset_retrieval", parameters={
-        "dataset_ids": [d.pid for d in job.datasetList or []],
-        "job_id": job.id
+        "dataset_ids": dataset_list,
+        "job_id": job_id
     }, timeout=0))
     return a
 
 
-async def run_create_dataset_deployment(dataset_id: int, file_size_MB: int, num_files: int):
-    a = await asyncio.create_task(run_deployment("create_test_dataset/dataset_creation", parameters={
-        "dataset_id": dataset_id,
+async def run_create_dataset_deployment(file_size_MB: int = 10, num_files: int = 10, datablock_size_MB: int = 20):
+    a = await asyncio.create_task(run_deployment("create_test_dataset/DEV_dataset_creation", parameters={
+        "num_files": num_files,
         "file_size_MB": file_size_MB,
-        "num_files": num_files
+        "datablock_size_MB": datablock_size_MB
     }, timeout=0))
     return a
 
 
-@router.get("/archivable_objects")
+@ router.get("/archivable_objects")
 def get_archivable_objects() -> list[StorageObject]:
     objects = S3Storage().list_objects(bucket=Bucket.staging_bucket())
     return [StorageObject(object_name=o.object_name or "") for o in objects]
 
 
-@router.get("/retrievable_objects")
+@ router.get("/retrievable_objects")
 def get_retrievable_objects() -> list[StorageObject]:
     objects = S3Storage().list_objects(bucket=Bucket.retrieval_bucket())
     return [StorageObject(object_name=o.object_name or "") for o in objects]
 
 
-@router.post("/new_dataset/")
-async def create_new_dataset(id: int):
+@ router.post("/new_dataset/")
+async def create_new_dataset():
     try:
-        m = await run_create_dataset_deployment(id, 10, 10)
-        return JSONResponse(content={f"status: flow scheduled '{m.name}'"}, status_code=200)
+        m = await run_create_dataset_deployment(file_size_MB=10, num_files=10, datablock_size_MB=20)
+        return JSONResponse(content={"name": m.name, "uuid": str(m.id)}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
-@router.post("/job/")
-async def create_job(job: Job):
+@ router.post("/jobs/")
+async def job_created(payload: Any = Body(None)):
     try:
-        j: Job = Job.model_validate(job)
-
-        m = None
-        match j.type:
+        id = payload['id']
+        type = payload['type']
+        match type:
             case "archive":
-                m = await run_archiving_deployment(j)
+                m = await run_archiving_deployment(job_id=id, dataset_list=[])
             case "retrieve":
-                m = await run_retrieval_deployment(j)
+                m = await run_retrieval_deployment(job_id=id, dataset_list=[])
             case _:
-                return JSONResponse(content={"error": f"unknown job type {j.type}"}, status_code=500)
+                return JSONResponse(content={"error": f"unknown job type {type}"}, status_code=500)
 
-        return JSONResponse(content={"status": f"{j.type} flow scheduled '{m.name}'"}, status_code=200)
+        return JSONResponse(content={"name": m.name, "uuid": str(m.id)}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
