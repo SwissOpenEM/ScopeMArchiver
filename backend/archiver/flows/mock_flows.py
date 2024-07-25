20c73a485d46f5386fb6b27fb7295bd91855b033
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-07-25 14:07:00 +0200

    WIP: dataset creation task adapted to scicat backend v4

--- backend/archiver/flows/mock_flows.py
+++ backend/archiver/flows/mock_flows.py
@@ -2,20 +2,23 @@ import random
 from prefect import flow, task
 from prefect.deployments.deployments import run_deployment
 import os
-import asyncio
+import datetime
 import shutil
 import requests
 from pathlib import Path
 from archiver.config.variables import Variables
+from archiver.config.blocks import Blocks
 from archiver.utils.datablocks import upload_objects_to_s3, create_tarballs
 from archiver.utils.working_storage_interface import Bucket
-from archiver.utils.model import OrigDataBlock, DataFile
+from archiver.utils.model import OrigDataBlock, DataFile, Dataset, DatasetLifecycle, Job, DatasetListEntry
 from archiver.flows.utils import StoragePaths
+from archiver.scicat.scicat_tasks import get_scicat_access_token
 from .task_utils import generate_task_name_dataset
 from archiver.flows.utils import StoragePaths
+from uuid import UUID
 
 
-@task(task_run_name=generate_task_name_dataset)
+@task(task_run_name=generate_task_name_dataset, persist_result=True, log_prints=True)
 def create_dummy_dataset(dataset_id: int, file_size_MB: int, num_files: int, datablock_size_MB: int):
     dataset_root = Variables().ARCHIVER_SCRATCH_FOLDER / str(dataset_id)
 
@@ -30,19 +33,103 @@ def create_dummy_dataset(dataset_id: int, file_size_MB: int, num_files: int, dat
     for i in range(num_files):
         os.system(f"dd if=/dev/urandom of={raw_files_folder}/file_{i}.bin bs={file_size_MB}M count=1 iflag=fullblock")
 
-    create_tarballs(dataset_id=dataset_id, src_folder=raw_files_folder,
-                    dst_folder=datablocks_folder, target_size=datablock_size_MB * (1024**2))
+    orig_data_blocks = create_tarballs(dataset_id=dataset_id, src_folder=raw_files_folder,
+                                       dst_folder=datablocks_folder, target_size=datablock_size_MB * (1024**2))
 
     files = upload_objects_to_s3(prefix=Path(StoragePaths.relative_origdatablocks_folder(dataset_id)),
                                  bucket=Bucket.landingzone_bucket(), source_folder=datablocks_folder, ext=".gz")
 
-    shutil.rmtree(dataset_root)
     checksums = []
+    for f in files:
+        checksums.append("1234")
 
-    # dataset = Dataset(
-    #     datasetlifecycle="",
-    #     updatedAt="",
-    # )
+    dataset = Dataset(
+        pid=str(dataset_id),
+        # createdAt=datetime.datetime.now().isoformat(),
+        principalInvestigator="testPI",
+        ownerGroup="ingestor",
+        owner="ingestor",
+        sourceFolder=str(dataset_root),
+        contactEmail="testuser@testfacility.com",
+        size=1234,
+        numberOfFiles=len(files),
+        creationTime=datetime.datetime.now().isoformat(),
+        type="raw",
+        creationLocation="ETHZ",
+        datasetlifecycle=DatasetLifecycle(
+            id=str(dataset_id),
+            archivable=True,
+            isOnCentralDisk=True
+        )
+        # origdatablocks=[origdatablock]
+    )
+    j = dataset.model_dump_json(exclude_none=True)
+
+    token = get_scicat_access_token()
+
+    # register orig datablocks
+    token_value = token.get_secret_value()
+    headers = {"Authorization": f"Bearer {token_value}", "Content-Type": "application/json"}
+    resp = requests.post(f"{Variables().SCICAT_ENDPOINT}{Variables().SCICAT_API_PREFIX}datasets/",
+                         data=j, headers=headers)
+    resp.raise_for_status()
+
+    import tarfile
+
+    for orig in orig_data_blocks:
+        with tarfile.open(orig, 'r') as file:
+            files_in_tar = file.getnames()
+            datafiles = [DataFile(
+                path=str(raw_files_folder / f),
+                chk="1234",
+                size=Path(raw_files_folder / f).stat().st_size,
+                time=str(datetime.datetime.now().isoformat())) for f in files_in_tar]
+
+            origdatablock = OrigDataBlock(
+                datasetId=str(dataset_id),
+                size=2 * file_size_MB,
+                ownerGroup="ingestor",
+                dataFileList=datafiles
+                # dataFileList=[DataFile(
+                #     path=str(p),
+                #     chk=c,
+                #     size=1111,
+                #     time=str(datetime.datetime.now().isoformat())) for p, c in zip(files, checksums)]
+            )
+
+            j = origdatablock.model_dump_json(exclude_none=True)
+
+            resp = requests.post(f"{Variables().SCICAT_ENDPOINT}{Variables().SCICAT_API_PREFIX}origdatablocks",
+                                 data=j, headers=headers)
+            resp.raise_for_status()
+
+    shutil.rmtree(dataset_root)
+
+    # create Job
+    datasetlist_entry = DatasetListEntry(pid=str(dataset_id), files=[])
+
+    job = Job(
+        jobParams={"username": "ingestor",
+                   "datasetList": f"[{datasetlist_entry.model_dump_json(exclude_none=True)}]"
+                   },
+        emailJobInitiator="testuser@testfacility.com",
+        type="archive",
+        ownerGroup="ingestor",
+        accessGroups=["ingestor"]
+    )
+    j = job.model_dump_json(exclude_none=True)
+    print(j)
+    resp = requests.post(f"{Variables().SCICAT_ENDPOINT}{Variables().SCICAT_API_PREFIX}jobs",
+                         data=j, headers=headers)
+    resp.raise_for_status()
+    # {"jobParams":{"username":"ingestor"},"emailJobInitiator":"scicatingestor@your.site","datasetList":[{"pid":"5244","files":[]}],"type":"archive"}
+    print(resp.json())
+    id = resp.json()["id"]
+
+    print(f"job id: {id}")
+    created_job = Job.model_validate(resp.json())
+    return created_job
+
     # {
     #     "ownerGroup": "ingestor",
     #     "accessGroups": [
@@ -178,21 +265,9 @@ def create_dummy_dataset(dataset_id: int, file_size_MB: int, num_files: int, dat
     #     ]
     # }
 
-    # create orig datablocks
-    origdatablock = OrigDataBlock(id=str(dataset_id),
-                                  datasetId=str(dataset_id),
-                                  size=2 * file_size_MB,
-                                  ownerGroup="0",
-                                  dataFileList=[DataFile(path=str(p), chk=c) for p, c in zip(files, checksums)]
-                                  )
-    j = origdatablock.model_dump_json()
 
-    # register orig datablocks
-    requests.post(f"{Variables().SCICAT_ENDPOINT}{Variables().SCICAT_API_PREFIX}OrigDatablocks/",
-                  data=j)
-
-
-@flow(name="create_test_dataset")
+@ flow(name="create_test_dataset", persist_result=True)
 def create_test_dataset_flow(file_size_MB: int = 10, num_files: int = 10, datablock_size_MB: int = 20):
     dataset_id = random.randint(0, 10000)
-    create_dummy_dataset(dataset_id, file_size_MB, num_files, datablock_size_MB)
+    job = create_dummy_dataset(dataset_id, file_size_MB, num_files, datablock_size_MB)
+    return dataset_id
