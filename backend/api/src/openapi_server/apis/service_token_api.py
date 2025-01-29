b30cb928e119e3a94b9958ea917617ea9ef2bf60
vermeul <swen@ethz.ch>
2025-01-29 17:51:28 +0100

    feat: add /token url to API and generate models and tests

---
+++ backend/api/src/openapi_server/apis/service_token_api.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+from typing import Dict, List  # noqa: F401
+import importlib
+import pkgutil
+
+from openapi_server.apis.service_token_api_base import BaseServiceTokenApi
+import openapi_server.impl
+
+from fastapi import (  # noqa: F401
+    APIRouter,
+    Body,
+    Cookie,
+    Depends,
+    Form,
+    Header,
+    HTTPException,
+    Path,
+    Query,
+    Response,
+    Security,
+    status,
+)
+
+from openapi_server.models.extra_models import TokenModel  # noqa: F401
+from openapi_server.models.create_service_token_resp import CreateServiceTokenResp
+from openapi_server.models.http_validation_error import HTTPValidationError
+from openapi_server.models.internal_error import InternalError
+from openapi_server.security_api import get_token_BearerAuth
+
+router = APIRouter()
+
+ns_pkg = openapi_server.impl
+for _, name, _ in pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + "."):
+    importlib.import_module(name)
+
+
+@router.post(
+    "/token",
+    responses={
+        201: {"model": CreateServiceTokenResp, "description": "Service Token created"},
+        422: {"model": HTTPValidationError, "description": "Validation Error"},
+        500: {"model": InternalError, "description": "Internal Server Error"},
+    },
+    tags=["service_token"],
+    summary="Create New Service Token",
+    response_model_by_alias=True,
+)
+async def create_new_service_token(
+    token_BearerAuth: TokenModel = Security(
+        get_token_BearerAuth
+    ),
+) -> CreateServiceTokenResp:
+    if not BaseServiceTokenApi.subclasses:
+        raise HTTPException(status_code=500, detail="Not implemented")
+    return await BaseServiceTokenApi.subclasses[0]().create_new_service_token()
---
+++ backend/api/src/openapi_server/apis/service_token_api_base.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from typing import ClassVar, Dict, List, Tuple  # noqa: F401
+
+from openapi_server.models.create_service_token_resp import CreateServiceTokenResp
+from openapi_server.models.http_validation_error import HTTPValidationError
+from openapi_server.models.internal_error import InternalError
+from openapi_server.security_api import get_token_BearerAuth
+
+class BaseServiceTokenApi:
+    subclasses: ClassVar[Tuple] = ()
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        BaseServiceTokenApi.subclasses = BaseServiceTokenApi.subclasses + (cls,)
+    async def create_new_service_token(
+        self,
+    ) -> CreateServiceTokenResp:
+        ...
