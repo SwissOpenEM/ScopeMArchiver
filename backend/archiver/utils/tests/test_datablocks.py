3d074c68881a5ce65189731858aaa46512b09454
Philipp Wissmann <philipp.wissmann@ethz.ch>
2025-06-18 15:09:31 +0200

    progress fix

--- backend/archiver/utils/tests/test_datablocks.py
+++ backend/archiver/utils/tests/test_datablocks.py
@@ -404,7 +404,7 @@ def mock_download_objects_from_s3(*args, **kwargs):
 @patch("archiver.utils.datablocks.download_object_from_s3", mock_download_objects_from_s3)
 def test_move_data_to_LTS(storage_paths_fixture, datablock_fixture):
     for datablock in datablock_fixture:
-        datablock_operations.move_data_to_LTS(mock_s3client(), test_dataset_id, datablock)
+        datablock_operations.move_data_to_LTS(test_dataset_id, datablock)
 
         file_in_lts = StoragePaths.lts_datablocks_folder(test_dataset_id) / Path(datablock.archiveId).name
 
